861e13a5d7def67eeb1de8eea311ee83b46a5972
andriy <and123bar@gmail.com>
2025-09-21 20:56:09 +0300

    bug2

--- js/task-1.js
+++ js/task-1.js
@@ -3,9 +3,6 @@ function slugify(title) {
 }
 
 
-
-
-
 console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
 console.log(slugify("English for developer")); // "english-for-developer"
 console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
--- js/task-3.js
+++ js/task-3.js
@@ -9,7 +9,6 @@ function filterArray(numbers, value) {
     }return filteredNumbers;
 }
 
-
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
 console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
 console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
